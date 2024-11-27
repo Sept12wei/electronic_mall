01bb531f2c0b4ac905c20744a9a3a6f49b9795e1
Sept12wei <161667697+Sept12wei@users.noreply.github.com>
2024-11-27 23:22:22 +0800

    Update vue.config.js

--- ElectronicMallVue/vue.config.js
+++ ElectronicMallVue/vue.config.js
@@ -1,8 +1,3 @@
-/*
- * @Description: 
- * @Author: 
- * @Date: 2024-03-26 15:27:05
- */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
