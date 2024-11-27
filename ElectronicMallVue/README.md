d790c63c4ac19d6e4fc2ad43a7f0d9a51d6cebcb
hww <2014613037@qq.com>
2024-11-27 22:52:28 +0800

    first commit

---
+++ ElectronicMallVue/README.md
@@ -0,0 +1,16 @@
+### 启动项目
+``` bash
+# 切换镜像
+npm config set registry https://registry.npm.taobao.org
+
+# 安装依赖，对应node -v 16.13.2
+npm install
+
+# 启动项目
+npm run dev
+
+
+# 学号
+202230444252
+# 姓名
+黄文蔚
---
+++ ElectronicMallVue/vue.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Description: 
+ * @Author: 
+ * @Date: 2024-03-26 15:27:05
+ */
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    port: 9192, // 端口
+    client: {
+      overlay: false,
+    },
+  },
+})
